c92af7a04ff41e656edbd2f5499b6bf5be890726
Arjanpeace <arjan.hoogenboom@student.uva.nl>
2019-09-03 16:48:21 +0200

    dinsdag

---
+++ src/blackjack/Deck.java
@@ -0,0 +1,18 @@
+package blackjack;
+
+import com.google.common.collect.ObjectArrays;
+import java.util.Arrays;
+import org.apache.commons.lang.ArrayUtils;
+
+public class Deck {
+	String[] kaartnummers = {"11", "2", "3", "4", "5", "6", "7", "8", "9", "10", "10", "10", "10"};
+	String[] kaartsymbool = {"H", "R", "S", "K"};
+	
+	for(int i=0; i<arr.length; i++){
+        System.out.println(arr[i]);
+	
+	
+	
+	
+}
+
--- src/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Blackjack! Let's have some fun");
-        System.out.println("The rules are simple, Y for another card and N for no card");
-        System.out.println("Good luck");
-    }
-}
